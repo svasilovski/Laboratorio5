b752015162d0fda73d793a545202a3b997769ff8
L <41869074+svasilovski@users.noreply.github.com>
2018-09-01 13:10:33 -0300

    Update subdivision.m

--- Laboratorio5/ListadoPaises/Model/subdivision.m
+++ Laboratorio5/ListadoPaises/Model/subdivision.m
@@ -2,7 +2,7 @@
 //  Subdivision.m
 //  Laboratorio5
 //
-//  Created by SAMUEL on 25/8/18.
+//  Created by SAMUEL on 1/9/18.
 //  Copyright © 2018 SAMUEL VASILOVSKI. All rights reserved.
 //
 
@@ -11,16 +11,24 @@
 @implementation Subdivision
 
 - (instancetype)initWithName:(NSString *)name
+                     details:(NSString *)details
+                       image:(UIImage *)image
 {
     self = [super init];
     if (self) {
         _name = name;
+        _details = details;
+        _image = image;
     }
     return self;
 }
 
-+ (Subdivision *)subdivisionWithName:(NSString *)name {
-    return [[Subdivision alloc] initWithName:name];
++ (Subdivision *)subdivisionWithName:(NSString *)name
+                             details:(NSString *)details
+                               image:(UIImage *)image {
+    return [[Subdivision alloc] initWithName:name
+                                     details:details
+                                       image:image];
 }
 
 @end
